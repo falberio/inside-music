b967ee8da9291b7bda96ddc0365d48e153b65346
Francisco Alberio <falberio@redmegatlon.local>
2020-12-08 14:45:59 -0300

    First commit

---
+++ src/components/Info.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+const Info = ({info}) => {
+
+    if(Object.keys(info).length === 0) return null;
+
+    const {
+            strArtistThumb: imagen,
+            strGenre: genero,
+            strBiographyES: biografia,
+            strFacebook: facebook,
+            strTwitter: twitter,
+            strLastFMChart: lastFM
+        } = info;
+
+    return ( 
+        <div className="card border-light">
+            <div className="card-header bg-primary text-light font-weight-bold">
+                Información artista
+            </div>
+            <div className="card-body">
+                <img src={imagen} alt="Logo Artista"/>
+                <p className="card-text font-weight-bold text-primary">{genero}</p>
+                <h2 className="card-text">Biografía:</h2>
+                <p className="card-text">{biografia}</p>
+                <p className="card-text">
+                    <a href={`https://${facebook}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-facebook"></i>
+                    </a>
+                    <a href={`https://${twitter}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-twitter"></i>
+                    </a>
+                    <a href={`${lastFM}`} target="_blank" rel="noopener noreferrer">
+                        <i className="fab fa-lastfm"></i>
+                    </a>
+                </p>
+            </div>
+        </div>
+     );
+}
+ 
+export default Info;
